9ea14f085f69355a01b6523a4728e9179b38258f
kirichenko87 <kappa134rus@gmail.com>
2024-10-22 21:37:01 +0300

    Второе задание

--- OOP_3.py
+++ OOP_3.py
@@ -27,9 +27,26 @@ class Patient:
               """)
 
 
-
-
-
+class  TouristSpot:
+    
+    def __init__(self, name_place: str,
+                 country: str,
+                 type_place: str
+                 ):
+        
+        self.__name_place = name_place
+        self.__country = country
+        self.__type_place = type_place
+    
+    def visit_place(self, name:str):
+        return f"Уважаемый {name} , вы посетили уникальное место {self.__name_place}"
+    
+    def __str__(self):
+        return f"""
+Страна: {self.__country}
+Туристическое место: {self.__name_place}
+Тип достопремичательности: {self.__type_place}
+    """
 
 class Program:
     
@@ -38,5 +55,9 @@ class Program:
         p = Patient("Роман", "Викторович", "Топоров", 49, "Притворство", )
         p.appointment_booking("13:30", "12/12/2024")
         print(p)
+        
+        t = TouristSpot("Озеро Байкал", "Россия", "Природа")
+        print(t)
+        
 
 Program.main()
